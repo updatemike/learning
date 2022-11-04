f52eeddf8111ac3356a1f894bd087c3b0430f98e
Michael-a-Maycock <115986306+michael-a-maycock@users.noreply.github.com>
2022-11-04 18:54:27 +0000

    Odin project rock-paper-scissors project.

---
+++ courses/the-odin-project/foundations/projects/rock-paper-scissors/script.js
@@ -0,0 +1,56 @@
+
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    /* Randomly return rock, paper or scissor */
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function getPlayerChoice() {
+    /* Prompt asking for player choice of either Rock, Paper or Scissors */
+    let playerChoice;
+    while (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissors") {
+        playerChoice = (prompt("Please choose Rock, Paper or Scissors.")).toLowerCase()
+    }
+    return playerChoice;
+}
+
+function playRound() {
+    /* Get computer and player choice and play a round of Rock,Paper,Scissors */
+    const computerChoice = getComputerChoice();
+    const playerChoice = getPlayerChoice();
+    if (computerChoice === playerChoice) {
+        return ["It was a tie. Both picked " + computerChoice.slice(0, 1).toUpperCase() + computerChoice.slice(1) + ".",
+            "t"]
+    } else if ((playerChoice === "rock" && computerChoice === "scissors") ||
+        (playerChoice === "scissors" && computerChoice === "paper") ||
+        (playerChoice === "paper" && computerChoice === "rock")) {
+        return (["You won! " + playerChoice.slice(0, 1).toUpperCase() + playerChoice.slice(1) + " beats " + computerChoice.slice(0, 1).toUpperCase() + computerChoice.slice(1) + ".",
+            "w"])
+    } else {
+        return ["You lost! " + computerChoice.slice(0, 1).toUpperCase() + computerChoice.slice(1) + " beats " + playerChoice.slice(0, 1).toUpperCase() + playerChoice.slice(1) + ".",
+            "l"]
+    }
+}
+
+function game() {
+    /* Play 5 rounds. Keep track of wins. Report winner and score at the end of 5 rounds. */
+    let computerScore = 0;
+    let playerScore = 0;
+    let nRounds = 1;
+    while (nRounds < 6) {
+        let round = playRound();
+        console.log(round[0]);
+        if (round[1] === "w")
+            playerScore++;
+        else if (round[1] === "l")
+            computerScore++;
+        nRounds++;
+    }
+    if (playerScore > computerScore)
+        return `You've won! Final score: Player ${playerScore} - ${computerScore} Computer`
+    else if (playerScore < computerScore)
+        return `You've Lost! Final score: Player ${playerScore} - ${computerScore} Computer`
+    else
+        return `It's a tie! Final score: Player ${playerScore} - ${computerScore} Computer`
+
+}
