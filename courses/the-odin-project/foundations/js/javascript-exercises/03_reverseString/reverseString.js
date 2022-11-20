bc1e6b6e28b05bae8eb8e005ad820a4280b310e0
updatemike <115986306+updatemike@users.noreply.github.com>
2022-11-20 14:31:59 +0000

    Full cleanup

--- courses/the-odin-project/foundations/js/javascript-exercises/03_reverseString/reverseString.js
+++
@@ -1,9 +0,0 @@
-const reverseString = function (inputString) {
-    let stringArray = inputString.split("");
-    let reversedArray = stringArray.reverse();
-    let outputString = reversedArray.join("");
-    return outputString;
-};
-
-// Do not edit below this line
-module.exports = reverseString;
--- practice/js/js2/main.js
+++
@@ -1,2 +0,0 @@
-/*  https://www.youtube.com/watch?v=hdI2bqOjy3c*/
-/* 1:11:38 */
