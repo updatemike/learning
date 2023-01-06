18a7a4444cdb94da2c98a5ac72cc97ed9391d220
Michael A <115986306+updatemike@users.noreply.github.com>
2023-01-06 20:24:33 +0000

    freeCodeCamp ES6 start

---
+++ Courses/Free Code camp/Curriculum/JavaScript Algorithms and Data Structures/02-js-es6/es6.js
@@ -0,0 +1,3 @@
+//LET AND VAR
+// var => global declaration, local if declared inside a function
+// let => acts like var but if declared inside a block, statement or expression, it's scope is limited to those.
