d6d951825fab561adaf39e8bbc599dbf091146b6
Michael A <115986306+updatemike@users.noreply.github.com>
2023-01-09 19:01:09 +0000

    FCC JS ES6 25/29

--- Courses/Free Code camp/Curriculum/JavaScript Algorithms and Data Structures/02-js-es6/es6.js
+++ Courses/Free Code camp/Curriculum/JavaScript Algorithms and Data Structures/02-js-es6/es6.js
@@ -79,7 +79,7 @@ const dFExample2 = {
 };
 
 //CLASSES => syntax to create objects.
-//class, new ClassName()
+// class, new ClassName()
 // Implicit constructor
 class ClassExample {
   constructor(classConstructorExample) {
@@ -100,3 +100,61 @@ const useClass2 = new ClassExample2(); //useClass2.classFunctionExample2() will 
 /* The class keyword declares a new function, to which a constructor is added. This constructor is invoked when new is called to create a new object.
  Note: UpperCamelCase should be used by convention for ES6 class names, as in SpaceShuttle used above.
  The constructor method is a special method for creating and initializing an object created with a class. */
+
+//GETTERS AND SETTERS
+// get, set
+// Getter functions are meant to simply return (get) the value of an object's private variable to the user without the user directly accessing the private variable.
+/* Setter functions are meant to modify (set) the value of an object's private variable based on the value passed into the setter function. 
+ This change could involve calculations, or even overwriting the previous value completely. */
+// Getters and setters are important because they hide internal implementation details.
+class GetSetClass {
+  constructor(argument) {
+    this._variableExample = argument;
+  }
+  //getter
+  get getSetArgument() {
+    this._variableExample;
+  }
+  //setter
+  set getSetArgument(updateGetSetArgument) {
+    this._variableExample = updateGetSetArgument;
+  }
+}
+const getSetExample = new GetSetClass("blue variable"); //console.log(getSetExample.getSetArgument) prints "blue variable"
+getSetExample.getSetArgument = "red variable"; ////console.log(getSetExample.getSetArgument) prints "red variable"
+// It is convention to precede the name of a private variable with an underscore (_). However, the practice itself does not make a variable private.
+
+//MODULE SCRIPT
+// <script type="module" src="filename.js"></script>;
+// Share code among JS files, this involved exporting parts and importing parts. You need to create a script in your HTML document with a type of module.
+//you use import and export for this.
+//named exports
+export const exportExample1 = (x, y) => {
+  return x + y;
+};
+export const exportExample2 = (x, y) => {
+  return x + y;
+};
+// can also not write export on each function you want to export and just do:
+export { exportExample1, exportExample2 };
+// Imagining that the exported functions above were in a file called export.js. To import:
+import { exportExample1, exportExample2 } from "./export.js"; // if I wanted to import just one function I'd just name that one
+// If you wish to import all the contents of a file into the current file use: import * as
+import * as myExportModule from "./export.js";
+/* The above import statement will create an object called myMathModule. This is just a variable name, you can name it anything. 
+ The object will contain all of the exports from math_functions.js in it, so you can access the functions like you would any other object property. 
+ Here's how you can use the add and subtract functions that were imported: */
+//myExportModule.exportExample1(1,2)
+//myExportModule.exportExample2(1,2)
+//export default => Usually you will use this syntax if only one value is being exported from a file. It is also used to create a fallback value for a file or module.
+export default function defaultFunctionName(x, y) {
+  return x + y;
+}
+//you don't need a named function.  You can use an anonymous function.
+/* Since export default is used to declare a fallback value for a module or file, you can only have one value be a default export in each module or file. 
+ Additionally, you cannot use export default with var, let, or const */
+//Import a default Export
+import defaultFunctionName from "./path_to_export.js";
+/* The syntax differs in one key place. 
+ The imported value, add, is not surrounded by curly braces ({}). add here is simply a variable name for whatever the default export of the math_functions.js file is. 
+ You can use any name here when importing a default. */
